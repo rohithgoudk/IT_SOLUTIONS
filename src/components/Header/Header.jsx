f9f25b40aab422772757767e49ef539c2febcdbb
Rohith Kappala <sa19agw>
2026-06-08 07:30:27 +0100

    Updated Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,7 +38,7 @@ function Header() {
       <nav>
 
       <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
-        <li><a href="#home">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#solutions">Solutions</a></li>
